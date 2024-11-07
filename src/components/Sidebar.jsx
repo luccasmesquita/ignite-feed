77d895a872ff82a29cbc501e501c1f7ea3e5039a
Luccas Mesquita <luccas.m.mesquita@hotmail.com>
2024-11-07 14:40:35 +0000

    finalizando sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,4 @@
+import { PencilLine } from 'phosphor-react';
 import styles from './Sidebar.module.css';
 
 export function Sidebar() {
@@ -9,12 +10,16 @@ export function Sidebar() {
         />
 
         <div className={styles.profile}>
+            <img className={styles.avatar} src="https://github.com/luccasmesquita.png" />
             <strong>Luccas Mesquita</strong>
             <span>Front-end Developer</span>
         </div>
-        
+
         <footer>
-            <a href="#">Editar seu perfil</a>
+            <a href="#">
+                < PencilLine size={20} />
+                Editar seu perfil
+            </a>
         </footer>
     </aside>
     );

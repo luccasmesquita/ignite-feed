237a7a3de6f39e41c11cbaba4c8cf8026c5a9ad3
Luccas Mesquita <luccas.m.mesquita@hotmail.com>
2024-11-14 20:43:19 +0000

    Inserindo comentários

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -11,10 +11,11 @@ import styles from './Post.module.css';
 
 export function Post({author, publishedAt, content}) {
     const [comments, setComments] = useState([
-        1,
-        2,
-        3
+        'Post muito bacana, hein!?'
     ]);
+
+    const [newCommentText, setNewCommentText] = useState('')
+    
     const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
         locale: ptBR,
       });
@@ -25,8 +26,13 @@ export function Post({author, publishedAt, content}) {
 
       function handleCreateNewComment() {
         event.preventDefault();
-        setComments([...comments, comments.length + 1]);
-        console.log(comments)
+
+        setComments([...comments, newCommentText]);
+        setNewCommentText('');
+      }
+
+      function handleNewCommentChange() {
+        setNewCommentText(event.target.value);
       }
 
     return(
@@ -55,7 +61,10 @@ export function Post({author, publishedAt, content}) {
             <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
                 <strong>Deixe seu feedback</strong>
                 <textarea 
+                    name='comment'
                     placeholder='Deixe um comentário'
+                    value={newCommentText}
+                    onChange={handleNewCommentChange}
                 />
                 <footer>
                     <button type='submit'>Publicar</button>
@@ -64,7 +73,7 @@ export function Post({author, publishedAt, content}) {
 
             <div className={styles.commentList}>
                {comments.map(comments => {
-                return <Comment />
+                return <Comment content={comments} />
                })}
             </div>
 
